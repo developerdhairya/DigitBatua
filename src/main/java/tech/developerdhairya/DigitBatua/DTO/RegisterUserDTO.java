096978b5f62cc3fb22da48d5471b3e61fdb27386
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-21 01:04:22 +0530

    add forgot passwd token entity

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/RegisterUserDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/RegisterUserDTO.java
@@ -12,5 +12,6 @@ public class RegisterUserDTO {
     private String email;
     private String password;
     private String confirmPassword;
+    private String mobileNumber;
 
 }
---
+++ src/main/java/tech/developerdhairya/DigitBatua/Entity/ForgotPasswordToken.java
@@ -0,0 +1,20 @@
+package tech.developerdhairya.DigitBatua.Entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data @Entity
+public class ForgotPasswordToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser userId;
+
+}
