db494f0d06f4b9d62a84319fc8c5007d71a97e51
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:09:17 +0530

    add NotFoundException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class NotFoundException extends Exception{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
