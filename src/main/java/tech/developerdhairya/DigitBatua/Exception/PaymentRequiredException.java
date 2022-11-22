5810aaf07ba847d39805164738f3a0801077b17b
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:09:37 +0530

    add PaymentRequiredException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/PaymentRequiredException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class PaymentRequiredException extends Exception{
+    public PaymentRequiredException(String message) {
+        super(message);
+    }
+}
