f09d519458b623c98a8f32ad49cb348096c3aa50
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:07:33 +0530

    Add ConflictException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/ConflictException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class ConflictException extends Exception{
+    public ConflictException(String message) {
+        super(message);
+    }
+}
