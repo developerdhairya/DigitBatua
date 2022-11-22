8c9cfdf725c7e4530ac4099be2e9709fdc25b25d
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:08:55 +0530

    Add NotAcceptableException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/NotAcceptableException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class NotAcceptableException extends Exception{
+    public NotAcceptableException(String message) {
+        super(message);
+    }
+}
