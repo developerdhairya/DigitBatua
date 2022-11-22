5493d6eba609d815cded1ac06fba5cfb43a08574
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:10:59 +0530

    add SecurityConfiguration.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package tech.developerdhairya.DigitBatua.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tech.developerdhairya.DigitBatua.JWTFilter;
+import tech.developerdhairya.DigitBatua.Service.UserDetailsServiceImpl;
+
+@EnableWebSecurity @Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    private JWTFilter jwtFilter;
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception {
+        security.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        security.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    //Configuring Authentication Manager
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl);
+    }
+
+    @Bean @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+}
