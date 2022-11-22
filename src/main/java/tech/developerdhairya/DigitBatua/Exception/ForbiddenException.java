c4cc9d6a05e12b719f327f9d678bb6fddac0b1a0
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:08:06 +0530

    Add ForbiddenException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class ForbiddenException extends Exception{
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
