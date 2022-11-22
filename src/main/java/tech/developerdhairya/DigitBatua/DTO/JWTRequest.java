9d7662785ebd1df2bb52f850c20b540eb92c4955
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-23 00:32:02 +0530

    update JWTRequest format

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/JWTRequest.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/JWTRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor
 public class JWTRequest {
 
-    private String username;
+    private String emailId;
     private String password;
 
 }
