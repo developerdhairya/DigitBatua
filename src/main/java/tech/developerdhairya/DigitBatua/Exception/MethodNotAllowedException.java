78d5d3422118bbe50aac27f829d7cdacf2b1e60a
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-22 21:08:30 +0530

    Add MethodNotAllowedException.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/Exception/MethodNotAllowedException.java
@@ -0,0 +1,7 @@
+package tech.developerdhairya.DigitBatua.Exception;
+
+public class MethodNotAllowedException extends Exception{
+    public MethodNotAllowedException(String message) {
+        super(message);
+    }
+}
