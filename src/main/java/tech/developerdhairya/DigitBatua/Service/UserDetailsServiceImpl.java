3e75f23c1d892e79435f165d4703c3d9c98a8cb8
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-23 00:31:21 +0530

    tackle userNameNotFoundException

--- src/main/java/tech/developerdhairya/DigitBatua/Service/UserDetailsServiceImpl.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Service/UserDetailsServiceImpl.java
@@ -1,18 +1,32 @@
 package tech.developerdhairya.DigitBatua.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import tech.developerdhairya.DigitBatua.Entity.AppUser;
+import tech.developerdhairya.DigitBatua.Repository.AppUserRepository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private AppUserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("admin", "$2a$11$Fiblmuqt1j7oWWiBOx23h.w3D6Xt.AsYYosB7Dr2HW8oapOaSceR6", new ArrayList<>());
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser user=userRepository.findByEmailId(email);
+        if(user==null){
+            throw new UsernameNotFoundException("User not Found");
+        };
+        return new User(email, user.getHashedPassword(), new ArrayList<>());
     }
 }
