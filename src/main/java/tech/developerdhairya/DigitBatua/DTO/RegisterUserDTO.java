93cf49fa5bcd2459149aa263c16431bf380931dc
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-24 20:55:03 +0530

    add Validation to RegisterUserDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/RegisterUserDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/RegisterUserDTO.java
@@ -4,14 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data @NoArgsConstructor @AllArgsConstructor
 public class RegisterUserDTO {
 
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String emailId;
+    @NotNull
     private String password;
+    @NotNull
     private String confirmPassword;
+    @NotNull
     private String mobileNumber;
 
 }
