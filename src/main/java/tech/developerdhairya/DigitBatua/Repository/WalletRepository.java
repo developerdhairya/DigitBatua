741c0df7c8f3a312bee112b9e4bd380f6c43d218
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-25 01:37:27 +0530

    update WalletRepository.java

--- src/main/java/tech/developerdhairya/DigitBatua/Repository/WalletRepository.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Repository/WalletRepository.java
@@ -2,10 +2,14 @@ package tech.developerdhairya.DigitBatua.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tech.developerdhairya.DigitBatua.Entity.AppUser;
 import tech.developerdhairya.DigitBatua.Entity.Wallet;
 
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    public Wallet findByAppUser(AppUser appUser);
+
 }
