87c8e29d89579813aec7466968ddc6afba246905
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-24 23:18:34 +0530

    add payment service tests

--- src/main/java/tech/developerdhairya/DigitBatua/Service/PaymentService.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Service/PaymentService.java
@@ -1,7 +1,12 @@
 package tech.developerdhairya.DigitBatua.Service;
 
+import com.razorpay.PaymentLink;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.json.JSONObject;
+import com.razorpay.Payment;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 
 @Service
 public class PaymentService {
@@ -12,8 +17,33 @@ public class PaymentService {
     @Value("${razorpay.key}")
     private String secret;
 
-    public void generatePaymentLink(){
-        System.out.println(key+" "+secret);
+    public String generatePaymentLink(int amount,String transactionId,String email) throws RazorpayException {
+        RazorpayClient razorpay = new RazorpayClient(key, secret);
+        JSONObject paymentLinkRequest = new JSONObject();
+        paymentLinkRequest.put("amount",amount);
+        paymentLinkRequest.put("currency","INR");
+        paymentLinkRequest.put("accept_partial",false);
+        paymentLinkRequest.put("first_min_partial_amount",amount);
+        paymentLinkRequest.put("expire_by",1691097057);
+        paymentLinkRequest.put("reference_id",transactionId);
+        paymentLinkRequest.put("description","Payment for transaction");
+        JSONObject customer = new JSONObject();
+        customer.put("name","null");
+        customer.put("contact","null");
+        customer.put("email",email);
+        paymentLinkRequest.put("customer",customer);
+        JSONObject notify = new JSONObject();
+        notify.put("sms",false);
+        notify.put("email",true);
+        paymentLinkRequest.put("notify",notify);
+        paymentLinkRequest.put("reminder_enable",true);
+        JSONObject notes = new JSONObject();
+        notes.put("null","null");
+        paymentLinkRequest.put("notes",notes);
+        paymentLinkRequest.put("callback_url","https://github.com/developerdhairya");
+        paymentLinkRequest.put("callback_method","get");
+        PaymentLink paymentLink = razorpay.paymentLink.create(paymentLinkRequest);
+        return paymentLink.toString();
     }
 
 
