28bce1448d732bb44324d0d3139c51cb226371d1
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-26 00:42:34 +0530

    add FundWalletDTO.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/FundWalletDTO.java
@@ -0,0 +1,10 @@
+package tech.developerdhairya.DigitBatua.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @Data @NoArgsConstructor
+public class FundWalletDTO {
+    private Integer amount;
+}
