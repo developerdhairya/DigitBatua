10651c28a00606f2a43fc5d03eb43f0db69d237b
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-26 00:43:17 +0530

    add WalletRepository.java

--- src/main/java/tech/developerdhairya/DigitBatua/Repository/WalletRepository.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Repository/WalletRepository.java
@@ -1,15 +1,22 @@
 package tech.developerdhairya.DigitBatua.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tech.developerdhairya.DigitBatua.Entity.AppUser;
 import tech.developerdhairya.DigitBatua.Entity.Wallet;
 
+import javax.validation.constraints.Email;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
-    public Wallet findByAppUser(AppUser appUser);
+//    @Query("select w from Wallet w where w.appUser = ?1")
+//    public Wallet findByAppUser(AppUser appUser);
 
+    @Query("select w from Wallet w where w.appUser.emailId = ?1")
+    public Wallet findByAppUser_EmailId(@Email(message = "Invalid emailId") String appUser_emailId);
+
+    
 }
