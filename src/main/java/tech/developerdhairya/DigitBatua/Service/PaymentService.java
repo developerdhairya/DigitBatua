946a9c7a8f7a4c9c116bd7a2e98a77d8628b81be
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-26 00:45:39 +0530

    update PaymentService.java

--- src/main/java/tech/developerdhairya/DigitBatua/Service/PaymentService.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Service/PaymentService.java
@@ -1,12 +1,17 @@
 package tech.developerdhairya.DigitBatua.Service;
 
 import com.razorpay.PaymentLink;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.json.JSONObject;
 import com.razorpay.Payment;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import tech.developerdhairya.DigitBatua.Entity.AppUser;
+import tech.developerdhairya.DigitBatua.Util.AuthenticationUtil;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PaymentService {
@@ -14,36 +19,46 @@ public class PaymentService {
     @Value("${razorpay.key}")
     private String key;
 
-    @Value("${razorpay.key}")
+    @Value("${razorpay.secret}")
     private String secret;
 
-    public String generatePaymentLink(int amount,String transactionId,String email) throws RazorpayException {
+    @Autowired
+    private AuthenticationUtil authenticationUtil;
+
+    public PaymentLink generatePaymentLink(AppUser appUser, int amount, String ref_id) throws RazorpayException {
         RazorpayClient razorpay = new RazorpayClient(key, secret);
         JSONObject paymentLinkRequest = new JSONObject();
         paymentLinkRequest.put("amount",amount);
         paymentLinkRequest.put("currency","INR");
         paymentLinkRequest.put("accept_partial",false);
         paymentLinkRequest.put("first_min_partial_amount",amount);
-        paymentLinkRequest.put("expire_by",1691097057);
-        paymentLinkRequest.put("reference_id",transactionId);
+        paymentLinkRequest.put("expire_by",authenticationUtil.calculateExpirationTime(24*60*150).toInstant().getEpochSecond());
+        paymentLinkRequest.put("reference_id",ref_id);
         paymentLinkRequest.put("description","Payment for transaction");
         JSONObject customer = new JSONObject();
-        customer.put("name","null");
-        customer.put("contact","null");
-        customer.put("email",email);
+        customer.put("name",appUser.getFirstName()+" "+appUser.getLastName());
+        customer.put("contact",appUser.getMobileNumber());
+        customer.put("email",appUser.getEmailId());
         paymentLinkRequest.put("customer",customer);
         JSONObject notify = new JSONObject();
-        notify.put("sms",false);
+        notify.put("sms",true);
         notify.put("email",true);
         paymentLinkRequest.put("notify",notify);
         paymentLinkRequest.put("reminder_enable",true);
         JSONObject notes = new JSONObject();
-        notes.put("null","null");
+//        notes.put("null","null");
         paymentLinkRequest.put("notes",notes);
-        paymentLinkRequest.put("callback_url","https://github.com/developerdhairya");
+        paymentLinkRequest.put("callback_url","http://localhost:4601/fundings/verify?fundingId="+ref_id);
         paymentLinkRequest.put("callback_method","get");
-        PaymentLink paymentLink = razorpay.paymentLink.create(paymentLinkRequest);
-        return paymentLink.toString();
+        return razorpay.paymentLink.create(paymentLinkRequest);
+    }
+
+    public boolean verifyPayment(String id,Integer amount) throws RazorpayException {
+        RazorpayClient razorpay = new RazorpayClient(key, secret);
+        PaymentLink paymentLink=razorpay.paymentLink.fetch(id);
+        Integer amountPaid=paymentLink.get("amount_paid");
+        System.out.println(amountPaid+" "+ (amountPaid.equals(amount)));
+        return amountPaid.equals(amount);
     }
 
 
