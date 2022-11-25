fc8bcb10029a0b09706ffdc5bb7a7dc95a98ee9d
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-26 00:42:53 +0530

    add FundWalletResponse.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/FundWalletResponse.java
@@ -0,0 +1,9 @@
+package tech.developerdhairya.DigitBatua.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor @Data
+public class FundWalletResponse {
+    private String paymentLink;
+}
