090d8785bfe89c46fddf61b85236fc47fe23b2af
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-28 02:25:03 +0530

    add ApproveMoneyRequestDTO.java

---
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/ApproveMoneyRequestDTO.java
@@ -0,0 +1,16 @@
+package tech.developerdhairya.DigitBatua.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApproveMoneyRequestDTO {
+    @NotBlank @NotNull
+    String requestId;
+}
