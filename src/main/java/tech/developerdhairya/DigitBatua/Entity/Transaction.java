d74a6d21e5a37740f1df0402fac72a297626dcb6
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-28 20:36:51 +0530

    update Transaction.java

--- src/main/java/tech/developerdhairya/DigitBatua/Entity/Transaction.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Entity/Transaction.java
@@ -2,12 +2,17 @@ package tech.developerdhairya.DigitBatua.Entity;
 
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -16,6 +21,7 @@ import java.util.UUID;
 @Setter
 @Entity
 @ToString
+@DynamicUpdate
 public class Transaction {
 
     @Id
@@ -43,5 +49,10 @@ public class Transaction {
 
     private String message;
 
+    @CreationTimestamp
+    private Timestamp creationTimestamp = Timestamp.valueOf(LocalDateTime.now());
+
+    @UpdateTimestamp
+    private Timestamp updateTimestamp;
 
 }
