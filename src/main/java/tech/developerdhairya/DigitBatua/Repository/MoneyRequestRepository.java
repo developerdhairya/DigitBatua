5854940ee8c1b0b5d036b873cf3fe79a78e0686b
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-28 20:36:36 +0530

    update MoneyRequestRepository

--- src/main/java/tech/developerdhairya/DigitBatua/Repository/MoneyRequestRepository.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Repository/MoneyRequestRepository.java
@@ -1,7 +1,9 @@
 package tech.developerdhairya.DigitBatua.Repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import tech.developerdhairya.DigitBatua.Entity.MoneyRequest;
 
@@ -9,13 +11,13 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface MoneyRequestRepository extends JpaRepository<MoneyRequest, UUID> {
+public interface MoneyRequestRepository extends PagingAndSortingRepository<MoneyRequest, UUID> {
 
     @Query("select m from MoneyRequest m where m.requestReceiverWallet.appUser.emailId = ?1")
-    public List<MoneyRequest> findAllByRequestReceiverWallet_AppUser_EmailId(String emailId);
+    public List<MoneyRequest> findAllByRequestReceiverWallet_AppUser_EmailId(String emailId, Pageable pageable);
 
     @Query("select m from MoneyRequest m where m.requesterWallet.appUser.emailId = ?1")
-    public List<MoneyRequest> findAllByRequesterWallet_AppUser_EmailId(String emailId);
+    public List<MoneyRequest> findAllByRequesterWallet_AppUser_EmailId(String emailId, Pageable pageable);
 
 
 }
