1c39d19210403fcc5691d0023b53dd79ddf5c9a3
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-28 20:37:59 +0530

    update WalletController.java

--- src/main/java/tech/developerdhairya/DigitBatua/Controller/WalletController.java
+++ src/main/java/tech/developerdhairya/DigitBatua/Controller/WalletController.java
@@ -15,6 +15,8 @@ import tech.developerdhairya.DigitBatua.Service.TransactionService;
 import tech.developerdhairya.DigitBatua.Service.WalletService;
 import tech.developerdhairya.DigitBatua.Util.TransactionType;
 
+import javax.validation.Valid;
+
 @RestController()
 @RequestMapping("/wallet")
 public class WalletController {
@@ -57,7 +59,7 @@ public class WalletController {
 
 
     @PostMapping("/fund")
-    public ResponseEntity<Object> fundWallet(@RequestBody FundWalletDTO fundWalletDTO) {
+    public ResponseEntity<Object> fundWallet(@Valid @RequestBody FundWalletDTO fundWalletDTO) {
         try {
             String emailId = SecurityContextHolder.getContext().getAuthentication().getName();
             FundWalletResponse response = walletService.fundWallet(emailId, fundWalletDTO.getAmount());
@@ -65,7 +67,7 @@ public class WalletController {
         } catch (BadRequestException e) {
             return ResponseHandler.generateErrorResponse(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getStackTrace());
             return ResponseHandler.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong!");
         }
     }
@@ -80,12 +82,13 @@ public class WalletController {
         } catch (PaymentRequiredException e) {
             return ResponseHandler.generateErrorResponse(HttpStatus.PAYMENT_REQUIRED, e.getLocalizedMessage());
         } catch (Exception e) {
+            System.out.println(e.getCause().getCause().getMessage());
             return ResponseHandler.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something Went Wrong!");
         }
     }
 
     @PutMapping("/sendMoney")
-    public ResponseEntity<Object> sendMoney(@RequestBody SendMoneyDTO sendMoneyDTO) {
+    public ResponseEntity<Object> sendMoney(@Valid @RequestBody SendMoneyDTO sendMoneyDTO) {
         try {
             return ResponseHandler.generateSuccessResponse(transactionService.sendMoney(sendMoneyDTO, TransactionType.DirectTransfer), HttpStatus.OK);
         } catch (PaymentRequiredException e) {
@@ -98,7 +101,7 @@ public class WalletController {
     }
 
     @PutMapping("/requestMoney")
-    public ResponseEntity<Object> requestMoney(@RequestBody MoneyRequestDTO moneyRequestDTO) {
+    public ResponseEntity<Object> requestMoney(@Valid @RequestBody MoneyRequestDTO moneyRequestDTO) {
         try {
             return ResponseHandler.generateSuccessResponse(transactionService.requestMoney(moneyRequestDTO), HttpStatus.OK);
         } catch (BadRequestException e) {
@@ -109,7 +112,7 @@ public class WalletController {
     }
 
     @PutMapping("/declineRequest")
-    public ResponseEntity<Object> declineRequest(@RequestBody DeclineMoneyRequestDTO dto) {
+    public ResponseEntity<Object> declineRequest(@Valid @RequestBody DeclineMoneyRequestDTO dto) {
         try {
             return ResponseHandler.generateSuccessResponse(transactionService.declineMoneyRequest(dto.getRequestId()), HttpStatus.OK);
         } catch (BadRequestException e) {
@@ -124,7 +127,7 @@ public class WalletController {
     }
 
     @PutMapping("/approveRequest")
-    public ResponseEntity<Object> approveRequest(@RequestBody ApproveMoneyRequestDTO dto) {
+    public ResponseEntity<Object> approveRequest(@Valid @RequestBody ApproveMoneyRequestDTO dto) {
         try {
             return ResponseHandler.generateSuccessResponse(transactionService.approveMoneyRequest(dto.getRequestId()), HttpStatus.OK);
         } catch (PaymentRequiredException e) {
@@ -139,20 +142,20 @@ public class WalletController {
     }
 
     @GetMapping("/getReceivedMoneyRequests")
-    public ResponseEntity<Object> getReceivedMoneyRequests(){
+    public ResponseEntity<Object> getReceivedMoneyRequests(@RequestParam Integer pageNumber,@RequestParam Integer pageSize){
         try{
             String emailId = SecurityContextHolder.getContext().getAuthentication().getName();
-            return ResponseHandler.generateSuccessResponse(transactionService.getAllReceivedMoneyRequests(emailId), HttpStatus.OK);
+            return ResponseHandler.generateSuccessResponse(transactionService.getAllReceivedMoneyRequests(emailId,pageNumber,pageSize), HttpStatus.OK);
         }catch (Exception e) {
             return ResponseHandler.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something Went Wrong!");
         }
     }
 
     @GetMapping("/getSentMoneyRequests")
-    public ResponseEntity<Object> getSentMoneyRequests(){
+    public ResponseEntity<Object> getSentMoneyRequests(@RequestParam Integer pageNumber,@RequestParam Integer pageSize){
         try{
             String emailId = SecurityContextHolder.getContext().getAuthentication().getName();
-            return ResponseHandler.generateSuccessResponse(transactionService.getAllSentMoneyRequests(emailId), HttpStatus.OK);
+            return ResponseHandler.generateSuccessResponse(transactionService.getAllSentMoneyRequests(emailId,pageNumber,pageSize), HttpStatus.OK);
         }catch (Exception e) {
             return ResponseHandler.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something Went Wrong!");
         }
