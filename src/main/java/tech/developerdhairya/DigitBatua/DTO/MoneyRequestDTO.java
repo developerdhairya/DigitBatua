06c0509b6d71e364a30b78ebe4e211b1f6cd39a1
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-28 20:36:18 +0530

    update MoneyRequestDTO

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/MoneyRequestDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/MoneyRequestDTO.java
@@ -6,12 +6,13 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class MoneyRequestDTO {
-    @Email
+    @Email @NotNull
     String requestReceiverEmailId;
 
     @Min(value = 1,message = "You can't request amount less than Rs.1")
