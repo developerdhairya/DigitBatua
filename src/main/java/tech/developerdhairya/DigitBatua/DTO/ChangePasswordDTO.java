f86ef5d3c34079eb386cd327e348e157f67eb4ce
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:50:50 +0530

    update ChangePasswordDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/ChangePasswordDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/ChangePasswordDTO.java
@@ -5,18 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangePasswordDTO {
-    @NotNull @Size(min = 8,max = 30,message = "Password should be between 8 and 30 characters")
+    @NotBlank
+    @NotEmpty
+    @Size(min = 8, max = 30, message = "Password should be between 8 and 30 characters")
     private String currentPassword;
-    @NotNull @Size(min = 8,max = 30,message = "Password should be between 8 and 30 characters")
+    @NotBlank
+    @NotEmpty
+    @Size(min = 8, max = 30, message = "Password should be between 8 and 30 characters")
     private String confirmCurrentPassword;
-    @NotNull @Size(min = 8,max = 30,message = "New Password should be between 8 and 30 characters")
+    @NotBlank
+    @NotEmpty
+    @Size(min = 8, max = 30, message = "New Password should be between 8 and 30 characters")
     private String newPassword;
 
 }
