a86e0662fcd3e32248957b607fde77fdd02e6da3
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:48:54 +0530

    update VerifyRegistrationDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/VerifyRegistrationDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/VerifyRegistrationDTO.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VerifyRegistrationDTO {
-    @NotNull
+    @NotBlank
+    @NotEmpty
     private String token;
 }
