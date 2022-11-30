07ecb73924cfccdced6253387bb299ab10df13ed
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:50:19 +0530

    update SendMoneyDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/SendMoneyDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/SendMoneyDTO.java
@@ -6,12 +6,16 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 
 public class SendMoneyDTO {
+    @NotBlank
+    @NotEmpty
     @Email
     private String receiverEmailId;
     @Min(value = 1,message = "You can't send amount less than Rs.1")
