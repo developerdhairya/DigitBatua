25339fd10cac736703d30a013bbf4236ad4f7de5
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:49:41 +0530

    update ResetPasswordByTokenDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/ResetPasswordByTokenDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/ResetPasswordByTokenDTO.java
@@ -6,23 +6,24 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-@AllArgsConstructor @NoArgsConstructor @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ResetPasswordByTokenDTO {
 
-    @Email
+    @Email @NotBlank @NotEmpty
     String emailId;
 
-    @NotBlank
+    @NotBlank @NotEmpty
     String token;
 
-    @NotBlank
-    @Size(min = 8, max = 30, message = "Password should be between 9 and 20 characters")
+    @NotBlank @NotEmpty @Size(min = 8, max = 30, message = "Password should be between 9 and 20 characters")
     String newPassword;
 
-    @NotBlank
-    @Size(min = 8, max = 30, message = "Password should be between 9 and 20 characters")
+    @NotBlank @NotEmpty @Size(min = 8, max = 30, message = "Password should be between 9 and 20 characters")
     String confirmNewPassword;
 
 }
