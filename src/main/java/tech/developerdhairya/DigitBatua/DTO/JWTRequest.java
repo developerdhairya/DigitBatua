87e80449d8674eddf40e75a37fa45a34bbafa792
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:51:54 +0530

    update JWTRequest.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/JWTRequest.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/JWTRequest.java
@@ -6,15 +6,16 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class JWTRequest {
-    @Email
+    @Email @NotBlank @NotEmpty
     private String emailId;
-    @NotBlank
+    @NotBlank @NotEmpty
     @Size(min = 8, max = 30, message = "Password should be between 8 and 30 characters")
     private String password;
 
