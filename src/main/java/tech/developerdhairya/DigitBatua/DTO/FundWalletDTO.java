739b70da086596ec1de0190902c545a0078f0c4d
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-11-30 18:51:41 +0530

    update FundWalletDTO.java

--- src/main/java/tech/developerdhairya/DigitBatua/DTO/FundWalletDTO.java
+++ src/main/java/tech/developerdhairya/DigitBatua/DTO/FundWalletDTO.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor @Data @NoArgsConstructor
 public class FundWalletDTO {
+    @NotNull
     @Min(value = 100,message = "You can't add less than Rs.1")
     private Integer amount;
 }
